e4c90f7f7208e84498dd857ae323c33bd488a62c
Sarthak <sarthaksri017@gmail.com>
2023-04-15 18:43:22 +0530

    ss

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const PORT = 80;
 const authRoute = require('./routes/auth.route');
 app.use("/auth", authRoute);
 
+app.get('/login',)
+
 app.listen(PORT, () => {
-    console.log("Server is running")
-})
+    console.log("Server is running");
+});
