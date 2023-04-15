03ea4766ccf0ee34849de1d83b899177bdceb004
Sarthak <sarthaksri017@gmail.com>
2023-04-15 14:34:26 +0530

    ss

--- routes/auth.login.route.js
+++ routes/auth.login.route.js
@@ -21,16 +21,13 @@ db.connect((err) => {
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
   
-    // Retrieve user data from database
     const sql = 'SELECT * FROM users WHERE username = ?';
     db.query(sql, [username], (err, result) => {
       if (err) throw err;
   
-      // Check if user exists
       if (result.length === 0) {
         res.status(401).json({ error: 'Invalid username or password' });
       } else {  
-          // Check if password is correct
           if (flag) {
             res.status(200).json({ message: 'Login successful' });
             res.redirect('/dashboard');
