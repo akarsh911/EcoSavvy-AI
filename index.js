007ebbad9f7903de61e6d9c026f127cbf385e5e9
akarsh <akarshagri121@gmail.com>
2023-04-15 19:10:44 +0530

    aks

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = 80;
+const PORT = 81;
 const authRoute = require('./routes/auth.route');
 app.use("/auth", authRoute);
-
-app.post('/login', (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    res.send(`Username: ${username} Password: ${password}`);
-  });
 
 app.listen(PORT, () => {
     console.log("Server is running");
